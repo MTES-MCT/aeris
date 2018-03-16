28878315aa90f1125a4f0cf453e04e012a552b0e
Keirua <Keirua@users.noreply.github.com>
2018-03-16 16:40:50 +0100

    feat/added Fabric deployment tool
    * first draft of a deployment task
    * added fabric documentation
    * update fabric script
    * Improved deployment

---
+++ documentation/deployment.md
@@ -0,0 +1,52 @@
+# Deployment
+
+Deployment is achieved using [fabric](http://www.fabfile.org/):
+
+_Fabric is a Python (2.5-2.7) library and command-line tool for streamlining the use of SSH for application deployment or systems administration tasks._
+
+On ubuntu, it can be installed globally with `apt-get install fabric`
+
+There is one task for deployments. It takes one parameter, the environment where we want to deploy.
+
+```bash
+$ fab deploy:staging
+$ fab deploy:prod
+```
+
+The staging environment is password protected, and has debugging tools enabled.
+The prod environment is, well, the production app.
+
+# Deployment structure
+
+Two environments, `staging` and `prod`, are hosted on the same machine for now.
+The `staging` environment is for 
+
+/var/www
+├── aeris-prod -> /home/deploy/prod/releases/deploy-1521128368
+└── aeris-staging -> /home/deploy/staging/releases/deploy-1521128490
+/home/deploy
+├── prod
+│   └── same structure as staging 
+└── staging
+    ├── releases 
+    │   ├── deploy-1521128488
+    │   └── deploy-1521128490
+    │       ├── bin
+    │       ├── composer.json
+    │       ├── composer.lock
+    │       ├── config
+    │       ├── public
+    │       ├── src
+    │       ├── symfony.lock
+    │       ├── templates
+    │       ├── var
+    │       └── vendor
+    └── shared
+        └── env
+
+The idea is as follow:
+
+ - /var/www/aeris-[env-name] is a symlink to a release, stored in /var/www/[env-name]/releases
+ - each release has a timestamp, and contains the complete project
+ - shared information (like environment variables), are stored in /var/www/[env]/shared. During deployment, they are copied in the release directory 
+
---
+++ fabfile.py
@@ -0,0 +1,52 @@
+from fabric.api import *
+from fabric.contrib.files import exists
+from fabric.contrib.project import *
+from time import time
+from sys import exit
+
+env.hosts = [
+    'ns388137.ip-176-31-252.eu'
+]
+env.user = 'root' # should be changed asap
+
+def deploy(env_name):
+    existing_environments = ['staging', 'prod']
+    if env_name not in existing_environments:
+        puts('%s is not a valid environment' % env_name)
+        puts('Allowed environments: %s' % ', '.join(existing_environments))
+        exit(1)
+
+    deploy_timestamp="%(time).0f" % {'time':time()}
+
+    deployment_directory='/home/deploy/%s/releases/release-%s' % (env_name, deploy_timestamp)
+    
+    upload_code(deployment_directory)
+    enable_project(env_name, deployment_directory)
+
+def upload_code(deployment_directory):
+    if not exists(deployment_directory):
+       puts('creating deployment directory')
+       run('mkdir -p %s' % deployment_directory) 
+
+    puts('Deploying project in %s' % deployment_directory)
+    rsync_project(
+        local_dir='./app/*',
+        remote_dir=deployment_directory,
+        default_opts='-zcrSLh --stats',
+        exclude=('.git', 'var')
+    )
+
+def enable_project(env_name, deployment_directory):
+    # Copy of the environment file
+    run('cp /home/deploy/%s/shared/env %s/.env' % (env_name, deployment_directory)) 
+    # warmup cache
+    with cd(deployment_directory):
+        run('bin/console cache:warmup --env=dev >> /dev/null')
+        run('bin/console cache:warmup --env=prod >> /dev/null')
+    # dump autoload
+
+    # Create symlink
+    run('ln -sfn %s /var/www/aeris-%s' % (deployment_directory, env_name))
+    # Restart the necessary services
+    run('service php7.0-fpm restart')
+    run('service nginx restart')
