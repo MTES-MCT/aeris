3a1b7dfbe874842e09220542f2570c72b7658d47
clemkeirua <clement@keiruaprod.fr>
2018-03-23 11:17:42 +0100

    first draft for the actual navigation once logged-in

---
+++ app/src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function dashboard()
+    {
+        return $this->render("inside/dashboard.html.twig");
+    }
+
+    public function declaration()
+    {
+        return $this->render("inside/declaration.html.twig");
+    }
+} 
