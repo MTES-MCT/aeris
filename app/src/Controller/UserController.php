e0e1f5496257232a37a2fa6ce3c66b413d5b7e70
clemkeirua <clement@keiruaprod.fr>
2018-03-27 09:55:28 +0200

    integrated the different screens

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -16,18 +16,33 @@ class UserController extends Controller
         return $this->render("inside/declaration.html.twig");
     }
 
+    public function historique()
+    {
+        return $this->render("inside/historique.html.twig");
+    }
+
     public function cr()
     {
         $period = new \DatePeriod(
              new \DateTime('2018-03-01'),
              new \DateInterval('P1D'),
-             new \DateTime('2018-03-31')
+             new \DateTime('2018-04-01')
         );
 
         $dates = [];
         foreach ($period as $key => $value) {
-            $dates[] = $value->format('Y-m-d');       
+            $dates[] = $value->format('d/m/Y');       
         }
-        return $this->render("inside/cr.html.twig", ['dateList' => $dates]);
+
+        $fluxCriterions = [ 'Poussières', 'CO', 'COT', 'Hcl', 'HF', 'SO2', 'Nox', 'NH3'];
+        $counterList = [ 'Poussières', 'CO', 'COT', 'HCl',  'HF', 'SO2', 'NOX', 'NH3', 'Total' ];
+        $concentrationList = [ 'Poussières', 'COT', 'HCl',  'HF', 'SO2', 'NOX', 'NH3', 'Total' ];
+
+        return $this->render("inside/cr.html.twig", [
+            'dateList' => $dates,
+            'fluxCriterions' => $fluxCriterions,
+            'counterList' => $counterList,
+            'concentrationList' => $concentrationList,
+        ]);
     }
 } 
