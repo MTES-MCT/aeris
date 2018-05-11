ba059feffaa7566665392d5611870296238b3ebf
clemkeirua <clement@keiruaprod.fr>
2018-05-11 11:31:58 +0200

    fixed date in form & xlsx templates -> xls

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,3 +1,3 @@
 require('../css/app.scss');
+
 import bulmaCalendar from '../../node_modules/bulma-extensions/bulma-calendar/dist/bulma-calendar.js';
-//import bulmaCalendar from './node_modules/bulma-calendar';
