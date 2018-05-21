7ca7a3e80a6586e3aa7f9e42c91d42d089f4246e
clemkeirua <clement@keiruaprod.fr>
2018-05-21 15:29:53 +0200

    cleanup fabfile

--- fabfile.py
+++ fabfile.py
@@ -57,6 +57,7 @@ def enable_project(env_name, deployment_directory):
 
     # Create symlink
     run('ln -sfn %s /var/www/aeris-%s' % (deployment_directory, env_name))
+    run('rm %s/public/build/declarations -rf' % (deployment_directory))
     run('ln -sfn /var/www/upload-%s %s/public/build/declarations' % (env_name, deployment_directory))
     # Restart the necessary services
     run('service php7.0-fpm restart')
