a1a3df69a009a0aba7a5901f1d0cfc4dae3cad69
clemkeirua <clement@keiruaprod.fr>
2018-05-25 16:15:38 +0200

    added test boilerplate

---
+++ app/tests/Twig/FrenchMonthExtensionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Twig;
+
+use PHPUnit\Framework\TestCase;
+use App\Twig\FrenchMonthExtension;
+
+class FrenchMonthExtensionTest extends TestCase
+{
+    public function testMonthfr() {
+    }
+}
+
+
