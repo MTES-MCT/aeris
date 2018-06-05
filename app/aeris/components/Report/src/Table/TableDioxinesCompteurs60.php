b858e0792a05e12caf852597c1da78ef2d800d0c
clemkeirua <clement@keiruaprod.fr>
2018-06-05 14:09:34 +0200

    better display of table

--- app/aeris/components/Report/src/Table/TableDioxinesCompteurs60.php
+++ app/aeris/components/Report/src/Table/TableDioxinesCompteurs60.php
@@ -15,40 +15,10 @@ class TableDioxinesCompteurs60 {
     public $dioxinesAnnuel;
 
     public function __construct($incinerateur, $ligneId) {
-        $this->compteur60DernierMois = 0;
         $this->dioxinesDernierMois = 0;
-        $this->compteur60Annuel = 0;
         $this->dioxinesAnnuel = 0;
+        $this->compteur60DernierMois = 0;
+        $this->compteur60Annuel = 0;
 
-        $this->analyzeDeclarations($incinerateur, $ligneId);
     }
-
-    private function analyzeDeclarations($incinerateur, $ligneId) {
-        $currentYear = DateUtils::getFirstOfJanuaryThisYear();
-        foreach($incinerateur->getDeclarationsIncinerateur() as $currDeclaration) {
-            $dateDeclaration = $currDeclaration->getDeclarationMonth();
-            if($dateDeclaration < $currentYear){
-                continue;
-            }
-            
-            foreach($currDeclaration->getDeclarationsFonctionnementLigne() as $declLigne) {
-                if($declLigne->getLigne()->getNumero() == $ligneId) {
-                    $this->extractCounterValues($declLigne->getMesuresCompteurs());
-                }
-            }
-        }
-    }
-
-    private function extractCounterValues($measures){
-        $currentYear = DateUtils::getFirstOfJanuaryThisYear();
-
-        foreach($measures as $measure) {
-            if($measure->getType() == self::MEASURE_COMPTEUR_60H && $measure->getComponent() == 'Total') {
-                $this->compteur60Annuel += $measure->getValue();
-            }
-            
-        }
-    }
-
-
 }
