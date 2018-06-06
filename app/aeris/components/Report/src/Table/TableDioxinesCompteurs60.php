373564a0f5060e513832d0372e21c9de52cdfed8
clemkeirua <clement@keiruaprod.fr>
2018-06-06 09:39:23 +0200

    extract dioxines annuel

--- app/aeris/components/Report/src/Table/TableDioxinesCompteurs60.php
+++ app/aeris/components/Report/src/Table/TableDioxinesCompteurs60.php
@@ -5,20 +5,28 @@ namespace Aeris\Component\Report\Table;
 use Aeris\Component\Report\DateUtils;
 
 class TableDioxinesCompteurs60 {
-
-    const MEASURE_COMPTEUR_60H = 'compt_art10_mensuel';
-
-    public $compteur60DernierMois;
     public $dioxinesDernierMois;
-    
-    public $compteur60Annuel;
     public $dioxinesAnnuel;
 
     public function __construct($incinerateur, $ligneId) {
         $this->dioxinesDernierMois = 0;
         $this->dioxinesAnnuel = 0;
-        $this->compteur60DernierMois = 0;
-        $this->compteur60Annuel = 0;
+        $this->analyzeDeclarations($incinerateur, $ligneId);
+    }
 
+    private function analyzeDeclarations($incinerateur, $ligneId) {
+        foreach($incinerateur->getDeclarationsDioxine() as $currDeclaration) {
+            $this->extractDioxinesValues($currDeclaration->getMesuresDioxine(), $ligneId);
+        }
+    }
+
+    private function extractDioxinesValues($measures, $ligneId){
+        $firstValidDate = DateUtils::getFirstOfJanuaryThisYear();
+
+        foreach($measures as $measure) {
+            if($measure->getDateDebut() >= $firstValidDate){
+                $this->dioxinesAnnuel += $measure->getValue();
+            }
+        }
     }
 }
