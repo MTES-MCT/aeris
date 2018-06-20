c9c10b7a02cb6c53e1d48e4612b9e386d55f9f19
clemkeirua <clement@keiruaprod.fr>
2018-06-20 16:58:01 +0200

    added missing default status migration for status

---
+++ app/src/Migrations/Version20180620145710.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180620145710 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql("update declaration_dioxines set status = 'validated' where status IS NULL;");
+        $this->addSql("update declaration_incinerateur set status = 'validated' where status IS NULL;");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
